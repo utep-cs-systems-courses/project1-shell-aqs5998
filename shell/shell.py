d35b352237292375261753903ff6403db51cb772
Alex Sanchez <alexsanchez@Alexs-Air.elp.rr.com>
2020-09-06 19:00:42 -0600

    making some tests

--- shell/shell.py
+++ shell/shell.py
@@ -6,3 +6,14 @@ if shellPromptToken is None:
     shellPrompt = str(input("?> ")) 
 else:
     shellPrompt = str(input(f"{shellPromptToken} "))
+    os.write(1, ("About to fork (pid:%d)\n" % pid).encode())
+
+    rc = os.fork()
+
+    if rc < 0:
+        os.write(2, ("fork failed, returning %d\n" % rc).encode())
+        sys.exit(1)
+    elif rc == 0:                   # child
+        os.write(1, ("I am child.  My pid==%d.  Parent's pid=%d\n" % (os.getpid(), pid)).encode())
+    else:                           # parent (forked ok)
+        os.write(1, ("I am parent.  My pid=%d.  Child's pid=%d\n" % (pid, rc)).encode())
