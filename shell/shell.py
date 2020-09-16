f569c69a013f5851e9ab4b79e41219fe412583b0
Alex Sanchez <alexsanchez@Alexs-Air.lan>
2020-09-15 22:59:56 -0600

    Updates

--- shell/shell.py
+++ shell/shell.py
@@ -9,8 +9,25 @@ while True:
         if 'PS1' in os.environ:
             os.write(1, (os.environ['PS1']).encode())
         else:
-            os.write(1, ('$$ ').encode())
+            os.write(1, ('$ ').encode())
             try:
                 userInput = input()
             except EOFError:
                 sys.exit(1)
+        if userInput == "": 
+            continue
+        """Exits the program"""
+        if 'exit' in userInput: 
+            print("see ya")
+            break
+        """Changes Directory"""
+        if 'cd' in userInput: 
+                if '..' in userInput:
+                    changeDir = '..'
+                else:
+                    changeDir = userInput.split('cd')[1].strip()
+                try:
+                    os.chdir(changeDir)
+                except FileNotFoundError:
+                    pass
+                continue
